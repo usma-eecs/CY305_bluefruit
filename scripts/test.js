e6c9bfa1509232b8dcf44d39481b4c2d452ce450
Jeremie Regnier <79362199+regnjere@users.noreply.github.com>
2022-03-01 15:35:57 -0500

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,5 @@
 $(function () {
-    $( "#Buttons" ).load("https://regnjere.github.io/CY105_Lab/lab code/Starter Code/Buttons.py.txt", function() {
+    $( "#Buttons" ).load("https://regnjere.github.io/CY105_Lab/lab code/Starter Code/Buttons.py", function() {
         alert( "Load was performed." );
     });
 });
