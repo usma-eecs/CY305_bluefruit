67d22a650e4dede683705e589db7b502d9d41cda
Jeremie Regnier <79362199+regnjere@users.noreply.github.com>
2022-03-01 16:17:42 -0500

    update test and py file names

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,7 @@
+const code_blocks = ["Buttons","Accelerometer","Capacitive Touch","LCD Bitmap"]//,"LCD Text and Background","LCD Turtle","Light Sensor","Microphone","NeoPixels","Servo","Speaker","Switch and red_LED","Temperature"]
 $(function () {
-    $( "#Buttons" ).load("https://raw.githubusercontent.com/regnjere/CY105_Lab/e6c9bfa1509232b8dcf44d39481b4c2d452ce450/lab%20code/Starter%20Code/Buttons.py");
+    for (let code of code_blocks) {
+        console.log(code);
+        $( "#"+code ).load("https://raw.githubusercontent.com/regnjere/CY105_Lab/e6c9bfa1509232b8dcf44d39481b4c2d452ce450/lab%20code/Starter%20Code/"+code+".py");
+    } 
 });
