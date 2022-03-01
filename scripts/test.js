7daf363bb3c9215365e4a35a089344585c69bcad
Jeremie Regnier <79362199+regnjere@users.noreply.github.com>
2022-03-01 16:29:15 -0500

    moved starter code files

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,7 @@
-const code_blocks = ["Accelerometer","Buttons","Capacitive_Touch","LCD_Bitmap","LCD Text and Background","LCD Turtle","Light Sensor","Microphone","NeoPixels","Servo","Speaker","Switch and red_LED","Temperature"]
+const code_blocks = ["Accelerometer","Buttons","Capacitive_Touch","LCD_Bitmap","LCD_Text_and_Background","LCD_Turtle","Light_Sensor","Microphone","NeoPixels","Servo","Speaker","Switch_and_red_LED","Temperature","Lab_Robot","Lab_Temp_Sensor"]
 $(function () {
     for (let code of code_blocks) {
-        console.log(code);
-        $( "#"+code ).load("https://raw.githubusercontent.com/regnjere/CY105_Lab/e6c9bfa1509232b8dcf44d39481b4c2d452ce450/lab%20code/Starter%20Code/"+code+".py");
+        $( "#"+code ).load("https://raw.githubusercontent.com/regnjere/CY105_Lab/main/lab%20code/"+code+".py");
     } 
+
 });
