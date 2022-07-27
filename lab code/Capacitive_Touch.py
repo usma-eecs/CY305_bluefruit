6210b7a2a1f54bc4805fa4e831ce99075b53744a
Regnier <regnjere@gmail.com>
2022-07-27 10:25:09 -0400

    Minor Lab Code Updates
    Updated Capacitive Touch with a delay

--- lab code/Capacitive_Touch.py
+++ lab code/Capacitive_Touch.py
@@ -1,8 +1,12 @@
 from adafruit_circuitplayground import cp
+import time
+
 
 while True:
     if cp.touch_A1: # Checks to see if A1 is being touched
         print("Touched pad A1")
     if cp.touch_A2:
         print("Touched pad A2")
+
+    time.sleep(0.1) # Waits for 1 second
 # Note that without the LCD attached, A3-A6 and the TX pads can be used the same way
