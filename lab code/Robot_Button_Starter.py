59aebf83c5fce17dae7ce4c0e31436c3badfba7c
Jeremie Regnier <79362199+regnjere@users.noreply.github.com>
2022-12-07 15:04:13 -0500

    Corrected robot button starter

--- lab code/Robot_Button_Starter.py
+++ lab code/Robot_Button_Starter.py
@@ -4,7 +4,7 @@ import board
 import pwmio
 from adafruit_motor import servo
 #Assigns pin A1 as a PWM output (controls the servo)
-pwm = pwmio.PWMOut(board.A1, frequency=50)
+pwm = pwmio.PWMOut(board.A1, frequency=25)
 
 # Create a servo object named my_servo.
 my_servo = servo.ContinuousServo(pwm)
@@ -13,11 +13,10 @@ my_servo = servo.ContinuousServo(pwm)
 
 # Modify the code below to spin the servo counterclockwise if button B is pressed, 
 # clockwise if button A is pressed, and not spinning if neither button is pressed
-my_servo.throttle = 0.5 # Spins the servo counterclockwise
-time.sleep(3)
-my_servo.throttle = 0.0 # Stops the servo
-time.sleep(3)
-my_servo.throttle = -0.5 # Spins the servo clockwise
+# You will need to use the code that's commented out in the 3 lines below
+#my_servo.throttle = 1 # Spins the servo counterclockwise
+#my_servo.throttle = 0.0 # Stops the servo
+#my_servo.throttle = -1 # Spins the servo clockwise
 
 
 while True:
@@ -26,6 +25,6 @@ while True:
         print("Button A pressed")
 
     # checks to see if button B is pressed
-    if cp.button_b:
+    elif cp.button_b:
         print("Button B pressed")
     time.sleep(0.1)
