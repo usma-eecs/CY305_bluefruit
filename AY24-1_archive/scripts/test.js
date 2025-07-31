2220a85d1abd180374031fbf1d40acec6f817a87
hTipp <hayden.tippett@gmail.com>
2025-07-31 12:47:03 -0400

    Delete AY24-1_archive directory

--- AY24-1_archive/scripts/test.js
+++
@@ -1,7 +0,0 @@
-//const code_blocks = ["Accelerometer","Buttons","Capacitive_Touch","LCD_Bitmap","LCD_Text_and_Background","LCD_Turtle","Light_Sensor","Microphone","NeoPixels","Servo","Speaker","Switch_and_red_LED","Temperature","Lab_Robot","Lab_Temp_Sensor"]
-$(function () {
-    for (let code of code_blocks) {
-        $( "."+code+"_code" ).load("https://raw.githubusercontent.com/regnjere/CY105_Lab/main/lab%20code/"+code+".py");
-    } 
-
-});
