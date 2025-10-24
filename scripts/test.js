035ab91aa21098d503c59fcdf8f2a8d50ee0cde7
brianpetty3 <brianpetty3@gmail.com>
2025-10-24 14:48:42 -0400

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,7 @@
 //const code_blocks = ["Accelerometer","Buttons","Capacitive_Touch","LCD_Bitmap","LCD_Text_and_Background","LCD_Turtle","Light_Sensor","Microphone","NeoPixels","Servo","Speaker","Switch_and_red_LED","Temperature","Lab_Robot","Lab_Temp_Sensor","bluetooth_example"]
 $(function () {
     for (let code of code_blocks) {
-        $( "."+code+"_code" ).load("https://raw.githubusercontent.com/regnjere/CY305_Lab/main/lab%20code/"+code+".py");
+        $( "."+code+"_code" ).load("https://raw.githubusercontent.com/brianpetty3/CY305_Lab/main/lab%20code/"+code+".py");
     } 
 
 });
